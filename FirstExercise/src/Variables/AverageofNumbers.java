3bec20ce91a1420860d89cd6779d3fd2fb75ac52
Yonites1 <yonites49@gmail.com>
2024-07-22 15:03:51 -0700

    Files have beeen added

---
+++ FirstExercise/src/Variables/AverageofNumbers.java
@@ -0,0 +1,19 @@
+package Variables;
+
+public class AverageofNumbers {
+	public static void main(String[] args) {
+        // Declare and initialize three integer variables
+        int num1 = 13;
+        int num2 = 34;
+        int num3 = 13;
+
+        // Calculate the sum of the numbers
+        int sum = num1 + num2 + num3;
+
+        // Calculate the average of the numbers
+        double average = sum / 3.0;
+
+        // Print the average
+        System.out.println("The average of the numbers is: " + average);
+    }
+}
---
+++ FirstExercise/src/Variables/MathOperations.java
@@ -0,0 +1,25 @@
+package Variables;
+//Math Operations: Declare two integer variables. Initialize them with values and then print out the sum, difference, multiplication, and division results.
+
+public class MathOperations {
+
+	public static void main(String[] args) {
+		 // Declare and initialize integer variables
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Calculate sum, difference, multiplication, and division
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+        
+        // Print the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+
+	}
+
+}
